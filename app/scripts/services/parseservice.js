82ddd1a221a5f6a0aa9263c348453d9d64e2d270
AbeHaruhiko <caliconography@gmail.com>
2015-05-28 15:41:56 +0900

    [add] parse.initialize

---
+++ app/scripts/services/parseservice.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name chronecoWebApp.ParseService
+ * @description
+ * # ParseService
+ * Service in the chronecoWebApp.
+ */
+angular.module('chroneco')
+  .service('ParseService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+
+  });
